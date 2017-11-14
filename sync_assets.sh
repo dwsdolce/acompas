f117a145011dcbb7c69ba13c02cc8770256acc81
Olivier Ricordeau <olivier@ricordeau.org>
2017-11-14 05:48:13 +0100

    Made a Cordova-based Android app, because the Crosswalk build chain is out of date. The Crosswalk webview is still used, however.

--- sync_assets.sh
+++ sync_assets.sh
@@ -30,28 +30,60 @@ rsync -av --delete ./common ./web
 rsync -av --delete ./bower_components ./web
 rsync -av ./images/icon_512x512.png ./web/images
 
-# Crosswalk version
+# Cordova version
 
 # Fonts
-rsync -av --delete --delete-excluded --exclude="common/fonts/Playball/OFL.txt" --exclude="common/audio/README" ./common ./crosswalk
+rsync -av --delete --delete-excluded --exclude="common/fonts/Playball/OFL.txt" --exclude="common/audio/README" ./common ./cordova/www
 # Bootstrap
-mkdir -p ./crosswalk/bower_components/bootstrap/dist/{css,fonts,js}
-rsync -av ./bower_components/bootstrap/dist/css/bootstrap.min.css ./crosswalk/bower_components/bootstrap/dist/css
-rsync -av --delete ./bower_components/bootstrap/dist/fonts ./crosswalk/bower_components/bootstrap/dist
-rsync -av ./bower_components/bootstrap/dist/js/bootstrap.min.js ./crosswalk/bower_components/bootstrap/dist/js
+mkdir -p ./cordova/www/bower_components/bootstrap/dist/{css,fonts,js}
+rsync -av ./bower_components/bootstrap/dist/css/bootstrap.min.css ./cordova/www/bower_components/bootstrap/dist/css
+rsync -av --delete ./bower_components/bootstrap/dist/fonts ./cordova/www/bower_components/bootstrap/dist
+rsync -av ./bower_components/bootstrap/dist/js/bootstrap.min.js ./cordova/www/bower_components/bootstrap/dist/js
 # jQuery
-mkdir -p ./crosswalk/bower_components/jquery/dist
-rsync -av ./bower_components/jquery/dist/jquery.min.js ./crosswalk/bower_components/jquery/dist
+mkdir -p ./cordova/www/bower_components/jquery/dist
+rsync -av ./bower_components/jquery/dist/jquery.min.js ./cordova/www/bower_components/jquery/dist
 # seiyria-bootstrap-slider
-mkdir -p ./crosswalk/bower_components/seiyria-bootstrap-slider/dist/css
-rsync -av ./bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js ./crosswalk/bower_components/seiyria-bootstrap-slider/dist
-rsync -av ./bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css ./crosswalk/bower_components/seiyria-bootstrap-slider/dist/css
+mkdir -p ./cordova/www/bower_components/seiyria-bootstrap-slider/dist/css
+rsync -av ./bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js ./cordova/www/bower_components/seiyria-bootstrap-slider/dist
+rsync -av ./bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css ./cordova/www/bower_components/seiyria-bootstrap-slider/dist/css
 # jquery-mousewheel
-mkdir -p ./crosswalk/bower_components/jquery-mousewheel
-rsync -av ./bower_components/jquery-mousewheel/jquery.mousewheel.min.js ./crosswalk/bower_components/jquery-mousewheel
+mkdir -p ./cordova/www/bower_components/jquery-mousewheel
+rsync -av ./bower_components/jquery-mousewheel/jquery.mousewheel.min.js ./cordova/www/bower_components/jquery-mousewheel
 # startaudiocontext
-mkdir -p ./crosswalk/bower_components/startaudiocontext
-rsync -av ./bower_components/startaudiocontext/StartAudioContext.js ./crosswalk/bower_components/startaudiocontext
+mkdir -p ./cordova/www/bower_components/startaudiocontext
+rsync -av ./bower_components/startaudiocontext/StartAudioContext.js ./cordova/www/bower_components/startaudiocontext
 # Icons
-mkdir -p ./crosswalk/images
-rsync -av ./images/icon_*.png ./crosswalk/images
+mkdir -p ./cordova/www/images
+rsync -av ./images/icon_*.png ./cordova/www/images
+# Icons used by Cordova when packaging the app
+rsync -av ./images/icon_36x36.png ./cordova/res/icon/android/icon-36-ldpi.png
+rsync -av ./images/icon_48x48.png ./cordova/res/icon/android/icon-48-mdpi.png
+rsync -av ./images/icon_72x72.png ./cordova/res/icon/android/icon-72-hdpi.png
+rsync -av ./images/icon_96x96.png ./cordova/res/icon/android/icon-96-xhdpi.png
+rsync -av ./images/icon_36x36.png ./cordova/platforms/android/res/mipmap-ldpi/icon.png
+rsync -av ./images/icon_48x48.png ./cordova/platforms/android/res/mipmap-mdpi/icon.png
+rsync -av ./images/icon_72x72.png ./cordova/platforms/android/res/mipmap-hdpi/icon.png
+rsync -av ./images/icon_96x96.png ./cordova/platforms/android/res/mipmap-xhdpi/icon.png
+rsync -av ./images/icon_96x96.png ./cordova/platforms/android/res/mipmap-xxhdpi/icon.png
+rsync -av ./images/icon_96x96.png ./cordova/platforms/android/res/mipmap-xxxhdpi/icon.png
+# Splash screen
+rsync -av ./images/icon_192x192.png ./cordova/res/screen/android/screen-ldpi-landscape.png
+rsync -av ./images/icon_192x192.png ./cordova/res/screen/android/screen-ldpi-portrait.png
+rsync -av ./images/icon_192x192.png ./cordova/res/screen/android/screen-mdpi-landscape.png
+rsync -av ./images/icon_192x192.png ./cordova/res/screen/android/screen-mdpi-portrait.png
+rsync -av ./images/icon_192x192.png ./cordova/res/screen/android/screen-hdpi-landscape.png
+rsync -av ./images/icon_192x192.png ./cordova/res/screen/android/screen-hdpi-portrait.png
+rsync -av ./images/icon_512x512.png ./cordova/res/screen/android/screen-xhdpi-landscape.png
+rsync -av ./images/icon_512x512.png ./cordova/res/screen/android/screen-xhdpi-portrait.png
+rsync -av ./images/icon_192x192.png ./cordova/platforms/android/res/drawable-land-ldpi/screen.png
+rsync -av ./images/icon_192x192.png ./cordova/platforms/android/res/drawable-port-ldpi/screen.png
+rsync -av ./images/icon_192x192.png ./cordova/platforms/android/res/drawable-land-mdpi/screen.png
+rsync -av ./images/icon_192x192.png ./cordova/platforms/android/res/drawable-port-mdpi/screen.png
+rsync -av ./images/icon_192x192.png ./cordova/platforms/android/res/drawable-land-hdpi/screen.png
+rsync -av ./images/icon_192x192.png ./cordova/platforms/android/res/drawable-port-hdpi/screen.png
+rsync -av ./images/icon_512x512.png ./cordova/platforms/android/res/drawable-land-xhdpi/screen.png
+rsync -av ./images/icon_512x512.png ./cordova/platforms/android/res/drawable-port-xhdpi/screen.png
+rsync -av ./images/icon_512x512.png ./cordova/platforms/android/res/drawable-land-xxhdpi/screen.png
+rsync -av ./images/icon_512x512.png ./cordova/platforms/android/res/drawable-port-xxhdpi/screen.png
+rsync -av ./images/icon_512x512.png ./cordova/platforms/android/res/drawable-land-xxxhdpi/screen.png
+rsync -av ./images/icon_512x512.png ./cordova/platforms/android/res/drawable-port-xxxhdpi/screen.png
