e30d6904eb8139bc5ae31180413b63e3683d9265
Olivier Ricordeau <olivier@ricordeau.org>
2020-12-20 12:42:47 +0100

    routes.js : handle 404s by redirecting to the app

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,11 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue')
+  },
+  // Redirect 404s to the app
+  {
+    path: '*',
+    component: () => import('layouts/MainLayout.vue')
   }
 ]
 
