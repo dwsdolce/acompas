80e4bfa8ccd120fa6f9a9e54772af5b7773f5f45
Olivier Ricordeau <olivier@ricordeau.org>
2021-01-23 15:43:31 +0100

    Commented a test which is not fully implemented.

--- test/cypress/integration/home/metronomePlugin.spec.js
+++ test/cypress/integration/home/metronomePlugin.spec.js
@@ -64,40 +64,40 @@ describe('Metronome plugin', () => {
       .should('include', 'A Compás')
   })
 
-  it('can prepare correct metronomeData', () => {
-    const audio = new Audio()
-    let audioFormat
-
-    if (audio.canPlayType('audio/flac')) {
-      audioFormat = 'flac'
-    } else if (audio.canPlayType('audio/mpeg')) {
-      audioFormat = 'mp3'
-    } else if (audio.canPlayType('audio/mp4')) {
-      audioFormat = 'mp4'
-    } else if (audio.canPlayType('audio/wav')) {
-      audioFormat = 'wav'
-    } else if (audio.canPlayType('audio/ogg')) {
-      audioFormat = 'ogg'
-    } else {
-      throw new Error('None of the available audio formats can be played')
-    }
-
-    const path = 'audio/'
-
-    forEachValue(audioSettings, (value, key) => {
-      for (let i = 0; i < value.length; i++) {
-        const url = path + value[i].src + '.' + audioFormat
-        cy.fixture(url, 'base64').then((sound) => {
-          const uri = `data:audio/${audioFormat};base64,`+ sound
-          const audio = new Audio(uri)
-
-          audio.play()
-        })
-      }
-    })
-
-    // for (let testObj in testData) {
-
-    // }
-  })
+  // it('can prepare correct metronomeData', () => {
+  //   const audio = new Audio()
+  //   let audioFormat
+  //
+  //   if (audio.canPlayType('audio/flac')) {
+  //     audioFormat = 'flac'
+  //   } else if (audio.canPlayType('audio/mpeg')) {
+  //     audioFormat = 'mp3'
+  //   } else if (audio.canPlayType('audio/mp4')) {
+  //     audioFormat = 'mp4'
+  //   } else if (audio.canPlayType('audio/wav')) {
+  //     audioFormat = 'wav'
+  //   } else if (audio.canPlayType('audio/ogg')) {
+  //     audioFormat = 'ogg'
+  //   } else {
+  //     throw new Error('None of the available audio formats can be played')
+  //   }
+  //
+  //   const path = 'audio/'
+  //
+  //   forEachValue(audioSettings, (value, key) => {
+  //     for (let i = 0; i < value.length; i++) {
+  //       const url = path + value[i].src + '.' + audioFormat
+  //       cy.fixture(url, 'base64').then((sound) => {
+  //         const uri = `data:audio/${audioFormat};base64,`+ sound
+  //         const audio = new Audio(uri)
+  //
+  //         audio.play()
+  //       })
+  //     }
+  //   })
+  //
+  //   // for (let testObj in testData) {
+  //
+  //   // }
+  // })
 })
