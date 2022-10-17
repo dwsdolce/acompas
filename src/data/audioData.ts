df9703b4f38fe82675068d956d94fac23d75fd01
ElJere <jeremiesieffert@hotmail.com>
2022-10-17 08:57:13 +0200

    Volume up

--- src/data/audioData.ts
+++ src/data/audioData.ts
@@ -2,147 +2,147 @@ export default {
   clara: [
     {
       src: 'clara/clara_1',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'clara/clara_2',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'clara/clara_3',
-      volume: 5,
+      volume: 7,
     },
   ],
   sorda: [
     {
       src: 'sorda/sorda_1',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'sorda/sorda_2',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'sorda/sorda_3',
-      volume: 5,
+      volume: 7,
     },
   ],
   cajon: [
     {
       src: 'cajon/cajon_1',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'cajon/cajon_2',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'cajon/cajon_3',
-      volume: 5,
+      volume: 7,
     },
   ],
   nudillo: [
     {
       src: 'nudillo/nudillo_1',
-      volume: 3,
+      volume: 5,
     },
     {
       src: 'nudillo/nudillo_2',
-      volume: -3,
+      volume: -1,
     },
   ],
   udu: [
     {
       src: 'udu/udu_1',
-      volume: 0,
+      volume: 2,
     },
     {
       src: 'udu/udu_2',
-      volume: -15,
+      volume: -13,
     },
     {
       src: 'udu/udu_3',
-      volume: 4,
+      volume: 6,
     },
   ],
   jaleo: [
     {
       src: 'jaleo/jaleo_1',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_2',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_3',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_4',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_5',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_6',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_7',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_8',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_9',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_10',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_11',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_12',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_13',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_14',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_15',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_16',
-      volume: -4,
+      volume: -2,
     },
     {
       src: 'jaleo/jaleo_17',
-      volume: -4,
+      volume: -2,
     },
   ],
   click: [
     {
       src: 'click/click_1',
-      volume: 5,
+      volume: 7,
     },
     {
       src: 'click/click_2',
-      volume: 5,
+      volume: 7,
     },
   ],
 }
