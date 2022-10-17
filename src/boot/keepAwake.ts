041b043ccaf96e2e4f309a1a7f641832249ab4f6
Pierre Mardon <pierre.mardon@gmail.com>
2022-10-17 15:51:53 +0200

    Add KeepAwake capacitor plugin to avoid screen dimming and stand by behaviors

---
+++ src/boot/keepAwake.ts
@@ -0,0 +1,11 @@
+import {KeepAwake} from '@capacitor-community/keep-awake';
+import { boot } from 'quasar/wrappers'
+
+export default boot(async () => {
+  if (await KeepAwake.isSupported()) {
+    await KeepAwake.keepAwake()
+    console.log('Keep awake enabled')
+  } else {
+    console.error('Keep awake not supported')
+  }
+})
