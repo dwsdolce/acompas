1117d1a156108e45828e12a864e75fb807626fc5
ElJere <jeremiesieffert@hotmail.com>
2024-05-07 01:43:48 +0200

    Small fix

--- src/components/PlayButton.vue
+++ src/components/PlayButton.vue
@@ -31,8 +31,12 @@ q-btn(
 </template>
 
 <style lang="sass" scoped>
+$shadow_1: 0 0 5px 5px rgba(0,0,0,0.4)
+$shadow_2: 0 0 15px 15px rgba(255,255,255,0.12)
+$shadow_3: inset 0 0 2px 2px rgba(255,255,255,0.2)
+
 #playBtn
   text-shadow: 0 0 15px rgba(0,0,0,0.5)
   &::before
-    box-shadow: 0 0 5px 5px rgba(0,0,0,0.5), 0 0 15px 15px rgba(255,255,255,0.2), inset 0 0 2px 2px rgba(255,255,255,0.3)
+    box-shadow: $shadow_1, $shadow_2, $shadow_3
 </style>
