a01f7a57bab696454a07759abb1babe98a83693b
ElJere <jeremiesieffert@hotmail.com>
2025-08-02 10:56:15 +0200

    Fixed little css bug

--- src/components/PlayButton.vue
+++ src/components/PlayButton.vue
@@ -38,18 +38,23 @@ q-btn(
 )
 </template>
 
-<style lang="sass" scoped>
-#playBtn
-  text-shadow: 0 0 5px rgba(0,0,0,0.5)
-  box-shadow: 0 0 5px rgba(0,0,0,0.8), 0 0 15px rgba(255, 255, 255, 0.25)
-  transition: all .5s ease-out
-  &::before
-    box-shadow: 0 0 5px rgba(0,0,0,0.8), 0 0 15px rgba(255, 255, 255, 0.25)
-    transition: all .5s ease-out
-  &:hover
-    box-shadow: 0 0 10px rgba(0,0,0,0.6), 0 0 20px rgba(255, 255, 255, 0.3)
-    &::before
-      box-shadow: 0 0 10px rgba(0,0,0,0.6), 0 0 20px rgba(255, 255, 255, 0.3)
-  &.light-mode::before
-    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.4), 0 3px 4px rgba(0, 0, 0, 0.28), 0 3px 3px -2px rgba(0, 0, 0, 0.24)
+<style lang="scss" scoped>
+#playBtn {
+  text-shadow: 0 0 5px rgba(0,0,0,0.5);
+  box-shadow: 0 0 5px rgba(0,0,0,0.8), 0 0 15px rgba(255, 255, 255, 0.25);
+  transition: all .5s ease-out;
+  &::before {
+    box-shadow: 0 0 5px rgba(0,0,0,0.8), 0 0 15px rgba(255, 255, 255, 0.25);
+    transition: all .5s ease-out;
+  }
+  &:hover {
+    box-shadow: 0 0 10px rgba(0,0,0,0.6), 0 0 20px rgba(255, 255, 255, 0.3);
+    &::before {
+      box-shadow: 0 0 10px rgba(0,0,0,0.6), 0 0 20px rgba(255, 255, 255, 0.3);
+    }
+  }
+  &.light-mode::before {
+    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.4), 0 3px 4px rgba(0, 0, 0, 0.28), 0 3px 3px -2px rgba(0, 0, 0, 0.24);
+  }
+}
 </style>
